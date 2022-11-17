9f10b9b34eddb1e0fe67f8be7864176107c65023
Susan Lee <93693073+007susan@users.noreply.github.com>
2022-11-16 17:16:59 -0800

    Create game.js

---
+++ rockpaperscissors/game.js
@@ -0,0 +1,66 @@
+const getUserChoice = (userInput) => {
+  //toLowerCase function makes the userInput all lowercase, incase people type in different capitalizations
+  userInput = userInput.toLowerCase();
+  if (
+    userInput === "rock" ||
+    userInput === "paper" ||
+    userInput === "scissors"
+  ) {
+    return userInput;
+  } else {
+    console.log("Invalid input");
+    return "Check your input";
+  }
+};
+
+function getComputerChoice() {
+  const choiceVariable = Math.floor(Math.random() * 3);
+  switch (choiceVariable) {
+    case 0:
+      return "rock";
+      break;
+    case 1:
+      return "paper";
+      break;
+    case 2:
+      return "scissors";
+      break;
+  }
+}
+
+const determineWinner = (userChoice, computerChoice) => {
+  if (userChoice === computerChoice) {
+    return "Game was a tie";
+  } else {
+    if (userChoice === "rock") {
+      if (computerChoice === "paper") {
+        return "Computer won";
+      } else if (computerChoice === "scissors") {
+        return "User won";
+      }
+    } else if (userChoice === "paper") {
+      if (computerChoice === "rock") {
+        return "User won";
+      } else if (computerChoice === "scissors") {
+        return "Computer won";
+      }
+    } else if (userChoice === "scissors") {
+      if (computerChoice === "rock") {
+        return "Computer won";
+      } else if (computerChoice === "paper") {
+        return "User won";
+      }
+    }
+  }
+};
+
+function playGame(input) {
+  const userChoice = getUserChoice(input);
+  const computerChoice = getComputerChoice();
+  console.log("User choice is: " + userChoice);
+  console.log("Computer choice is: " + computerChoice);
+  console.log(determineWinner(userChoice, computerChoice));
+}
+
+// User can replace scissors with rock or paper
+playGame("Scissors");
